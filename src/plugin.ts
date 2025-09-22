a35f78a6e0311c1093adbdf1b685e8287f293472
Tom Kregenbild <tommusicproject@gmail.com>
2025-09-22 22:57:54 +0300

    refactor: remove Notion Today Dial action and related files

--- src/plugin.ts
+++ src/plugin.ts
@@ -2,14 +2,12 @@ import streamDeck, { LogLevel } from "@elgato/streamdeck";
 
 import { ActiveTasksDialAction } from "./actions/active-tasks-dial";
 import { NotionTodayAction } from "./actions/notion-today";
-import { NotionTodayDialAction } from "./actions/notion-today-dial";
 
 // We can enable "trace" logging so that all messages between the Stream Deck, and the plugin are recorded. When storing sensitive information
 streamDeck.logger.setLevel(LogLevel.TRACE);
 
 // Register the Notion Today action.
 streamDeck.actions.registerAction(new NotionTodayAction());
-streamDeck.actions.registerAction(new NotionTodayDialAction());
 streamDeck.actions.registerAction(new ActiveTasksDialAction());
 
 // Finally, connect to the Stream Deck.
